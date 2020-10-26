bb8aea11177008e9e485edbaf8df431c087b7e5e
NachoAG <ignacio.arenas.guerra@gmail.com>
2020-10-26 16:51:20 +0100

    Updated csv headers to the results file

--- laliga_scrapping_dynamic.py
+++ laliga_scrapping_dynamic.py
@@ -36,6 +36,7 @@ def main():
             # Wait for the results to be loaded 
             matches = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'final')))
             # Go throught the week matches
+            file.write("LOCAL_TEAM, LOCAL_SCORE, VISITING_TEAM, VISITING_SCORE\n")
             for match in matches[::2]:
                 info = match.find_elements_by_tag_name('a')
                 teams, score = info
